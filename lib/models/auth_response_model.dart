598e24d3e21c99f4af7892d9268a8cbbccd5243f
Julien Dhyne <julien.dhyne@gmail.com>
2024-12-19 08:58:35 +0100

    add basic models

---
+++ lib/models/auth_response_model.dart
@@ -0,0 +1,24 @@
+import 'user_model.dart';
+
+class AuthResponse {
+  bool success;
+  String message;
+  User? user;
+  String? token;
+
+  AuthResponse({
+    required this.success,
+    required this.message,
+    required this.user,
+    required this.token,
+  });
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return AuthResponse(
+      success: json['success'],
+      message: json['message'],
+      user: json['data'] != null ? User.fromJson(json['data']) : null,
+      token: json['token'],
+    );
+  }
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,10 @@
+class User {
+  final int id;
+  final String email;
+
+  const User({required this.id, required this.email});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(id: json['id'], email: json['email']);
+  }
+}
